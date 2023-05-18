a7d463eecaae3a7cedeb74f9789efd42d6201487
DamaniN <37876601+DamaniN@users.noreply.github.com>
2023-05-18 12:32:50 -0700

    added custom providers

--- deploy/terraform/eks/default/providers.tf
+++ deploy/terraform/eks/default/providers.tf
@@ -12,9 +12,25 @@ terraform {
       version = "2.9.0"
     }
   }
+  backend "s3" {
+    profile              = "test-test-account"
+    bucket               = "xspm-poc-terraform-backend-state"
+    key                  = "aws_retail-store-sample-app.tfstate"
+    region               = "us-west-2"
+
+    dynamodb_table = "xspm-poc-terraform-states-lock-table"
+  }
 }
 
 provider "aws" {
+  profile = "${terraform.workspace}-test-account"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias = "secrets"
+  profile = "test-test-account"
+  region = "us-west-2"
 }
 
 provider "kubernetes" {
@@ -37,4 +53,16 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
     cluster_ca_certificate = base64decode(module.retail_app_eks.cluster_certificate_authority_data)
   }
+}
+
+
+provider "aws" {
+  profile = "${terraform.workspace}-test-account"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias = "secrets"
+  profile = "test-test-account"
+  region = "us-west-2"
 }
